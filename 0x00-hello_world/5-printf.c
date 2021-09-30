c3aa390202a4fb6133ae66eed693551b2bcb4964
WILLOT Philippe <willot.ph@gmail.com>
2021-09-30 09:40:25 +0000

    Add script 5 printf function

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+  * main - Prints text using the function printf
+  * Return: 0 everything was a success
+  */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
