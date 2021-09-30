c395fabebc498813493162a0f71b9bd42c1c344c
WILLOT Philippe <willot.ph@gmail.com>
2021-09-30 13:37:55 +0000

    Fixing trailing whitespace betty

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,7 +7,8 @@
 
 int main(void)
 {
-	char sentence[] = "and that piece of art is useful\" - DoraKorpar, 2015-10-19\n";
+	char sentence[] = "and that piece of art is useful\" - Dora\
+Korpar, 2015-10-19\n";
 
 	fwrite(sentence, sizeof(sentence) - 1, 1, stdout);
 	return (1);
