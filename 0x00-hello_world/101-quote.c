0b7a0b32aaa186819e5cd8e7b2cdb3c96b1cd0cc
WILLOT Philippe <willot.ph@gmail.com>
2021-09-30 13:21:40 +0000

    Script who print text using fwrite and avoiding printf or puts

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+  * main - Entry point - Prints a sentence
+  * Return: Always 1 Success
+  */
+
+int main(void)
+{
+	char sentence[] = "and that piece of art is useful\" - Dora
+		Korpar, 2015-10-19\n";
+
+	fwrite(sentence, sizeof(sentence) - 1, 1, stdout);
+	return (1);
+}
