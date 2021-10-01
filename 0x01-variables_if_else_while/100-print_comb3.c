54397633edc8d1c3757224ac02993a7952298d51
WILLOT Philippe <willot.ph@gmail.com>
2021-10-01 13:41:04 +0000

    Ajout du script 100: Prints all possible different combinations of two digits

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+/**
+  * main - Entry point
+  *
+  * Description: Print all possible different combinations of two digits
+  *
+  * Return: Always 0 (success)
+  */
+
+int main(void)
+{
+
+	int firstDigit = 0, secondDigit = 1, modifier = 2;
+
+	while (firstDigit < 9)
+	{
+		while (secondDigit <= 9)
+		{
+			putchar('0' + firstDigit);
+			putchar('0' + secondDigit);
+			if (firstDigit != 8 || secondDigit != 9)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+			secondDigit++;
+		}
+
+	secondDigit = modifier;
+	modifier++;
+	firstDigit++;
+	}
+
+	putchar('\n');
+	return (0);
+}
