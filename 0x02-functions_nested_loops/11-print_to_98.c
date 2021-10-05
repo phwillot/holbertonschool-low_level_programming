9b752777525a9daed5bcd7db1f3172f6bb23c5ba
WILLOT Philippe <willot.ph@gmail.com>
2021-10-05 17:28:39 +0000

    Add 2 digits number negative

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -8,7 +8,7 @@
 
 void print_to_98(int n)
 {
-	if (n >= 98)
+	if (n >= 98 && n > 0)
 	{
 		while (n >= 98)
 		{
@@ -31,11 +31,11 @@ void print_to_98(int n)
 			if (n < 0)
 			{
 				_putchar('-');
-				if (n == -10)
+				if (n <= -10)
 					_putchar((n - (n * 2)) / 10 + '0');
 				_putchar((n - (n * 2)) % 10 + '0');
 			}
-			if (n > 10)
+			if (n >= 10)
 				_putchar(n / 10 + '0');
 			if (n >= 0)
 				_putchar(n % 10 + '0');
