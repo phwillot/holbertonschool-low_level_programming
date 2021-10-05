4ce312819959bd7a1a03e5ef7aac91aeb753a741
WILLOT Philippe <willot.ph@gmail.com>
2021-10-05 17:39:15 +0000

    Another fix on digits

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -32,8 +32,9 @@ void print_to_98(int n)
 			{
 				_putchar('-');
 				if (n <= -100)
+					_putchar((n - (n * 2)) / 100 + '0');
+				if (n <= -10)
 					_putchar((n - (n * 2)) / 10 % 10 + '0');
-				_putchar((n - (n * 2)) / 10 + '0');
 				_putchar((n - (n * 2)) % 10 + '0');
 			}
 			if (n >= 10)
