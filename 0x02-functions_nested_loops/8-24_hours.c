36a743288d80187a58d556b31f46a1745ac57341
WILLOT Philippe <willot.ph@gmail.com>
2021-10-05 11:37:42 +0000

    AJout script jack_bauer (compte les heures)

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+  * jack_bauer - Prints every minute of the day of the Jack Bauer
+  * starting from 00:00 to 23:59
+  *
+  */
+
+void jack_bauer(void)
+{
+	int a = 48, b = 48, c = 48, d = 48;
+
+	while (a < 50 || b < 52)
+	{
+		while (c < 54 && d < 58)
+		{
+			_putchar(a);
+			_putchar(b);
+			_putchar(':');
+			_putchar(c);
+			_putchar(d);
+			if (c != 54 || d != 58)
+				_putchar('\n');
+
+			d == 57 ? d = 48, c++ : d++;
+		}
+		d = c = 48;
+		b == 57 ? b = 48, a++ : b++;
+	}
+
+
+}
