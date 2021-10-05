2092dd040a1e256aafdfa53060852d1d4fcecb68
WILLOT Philippe <willot.ph@gmail.com>
2021-10-05 17:34:35 +0000

    Fixed 3 digits negative

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -31,8 +31,9 @@ void print_to_98(int n)
 			if (n < 0)
 			{
 				_putchar('-');
-				if (n <= -10)
-					_putchar((n - (n * 2)) / 10 + '0');
+				if (n <= -100)
+					_putchar((n - (n * 2)) / 10 % 10 + '0');
+				_putchar((n - (n * 2)) / 10 + '0');
 				_putchar((n - (n * 2)) % 10 + '0');
 			}
 			if (n >= 10)
