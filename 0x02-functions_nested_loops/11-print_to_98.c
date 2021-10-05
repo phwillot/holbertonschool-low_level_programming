54f40886d9355b63f34306ac0c9f6deb8135056b
WILLOT Philippe <willot.ph@gmail.com>
2021-10-05 17:20:07 +0000

    Added programm 11 (98)

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,51 @@
+#include "main.h"
+
+/**
+  * print_to_98 - Prints all natural numbers from n to 98
+  * @n: Starting number to print all the way to 98
+  *
+  */
+
+void print_to_98(int n)
+{
+	if (n >= 98)
+	{
+		while (n >= 98)
+		{
+			if (n >= 100)
+				_putchar(n / 100 + '0');
+			_putchar(n / 10 % 10 + '0');
+			_putchar(n % 10 + '0');
+			if (n != 98)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+			n--;
+		}
+	}
+	else
+	{
+		while (n <= 98)
+		{
+			if (n < 0)
+			{
+				_putchar('-');
+				if (n == -10)
+					_putchar((n - (n * 2)) / 10 + '0');
+				_putchar((n - (n * 2)) % 10 + '0');
+			}
+			if (n > 10)
+				_putchar(n / 10 + '0');
+			if (n >= 0)
+				_putchar(n % 10 + '0');
+			if (n != 98)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+			n++;
+		}
+	}
+	_putchar('\n');
+}
