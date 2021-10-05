682faf51ec39211c7004464e35ecff1ef4e75c96
WILLOT Philippe <willot.ph@gmail.com>
2021-10-05 18:21:19 +0000

    Ajout du programme 101-natural.c

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+  * main - Entry Point
+  *
+  * Description: Prints the sum of all the multiples of 3 or 5 below 1024
+  *
+  * Return: Always 0 (Success)
+  */
+
+int main(void)
+{
+	int cur = 0, sum = 0;
+
+	while (cur < 1024)
+	{
+		if (cur % 3 == 0)
+			sum += cur;
+		if (cur % 5 == 0)
+			sum += cur;
+		cur++;
+	}
+
+	printf("%d\n", sum);
+
+	return (0);
+}
