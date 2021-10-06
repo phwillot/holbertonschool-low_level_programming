73715b383b46c1396c167a5a4c96a8cf692db2f5
WILLOT Philippe <willot.ph@gmail.com>
2021-10-06 13:38:23 +0000

    Added program 102 fibonacci

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+  * main - Entry point
+  *
+  * Description: Prints the first 50 Fibonacci numbers, starting with 1 and 2
+  *
+  * Return: Always 0 (Success)
+  */
+
+int main(void)
+{
+	int i, y = 0, z = 1, number;
+
+	for (i = 0; i < 50; i++)
+	{
+		number = y + z;
+		if (i != 49)
+			printf("%d, ", number);
+		else
+			printf("%d\n", number);
+		y = z;
+		z = number;
+	}
+	return (0);
+}
