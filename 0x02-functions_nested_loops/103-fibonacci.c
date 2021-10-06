5fc016da4eb901b2f1c1864d88ac43dfc265ce13
WILLOT Philippe <willot.ph@gmail.com>
2021-10-06 18:45:08 +0000

    Added 103 fibonacci

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+/**
+  * main - Entry point
+  *
+  * Description: Prints Fibonacci number until 4 000 000 and add the sum of
+  * even-valued terms.
+  *
+  * Return: Always 0 (Success)
+  */
+
+int main(void)
+{
+	long int i, y = 0, z = 1, number, sum = 0;
+
+	for (i = 0; i < 50; i++)
+	{
+		number = y + z;
+		if (number > 4000000)
+			break;
+
+		if (number % 2 == 0)
+		{
+			sum += number;
+			printf("%ld\n", sum);
+		}
+		y = z;
+		z = number;
+	}
+	printf("%ld\n", sum);
+	return (0);
+}
