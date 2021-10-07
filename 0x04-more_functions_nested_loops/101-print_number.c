13c91017a1d64cdeeda86b75f5c1eda511c82c3f
WILLOT Philippe <willot.ph@gmail.com>
2021-10-07 14:12:04 +0000

    Update program 101

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -9,11 +9,23 @@
 
 void print_number(int n)
 {
-	if (n >= 0)
+	if (n == 0)
+		_putchar('0' + n);
+
+	else
 	{
-		while (n >= 10)
+		if (n < 0)
 		{
-			n /= 10;
+			n -= n * 2;
+			_putchar('-');
 		}
+			if (n >= 1000)
+				_putchar(n / 1000 + '0');
+			if (n >= 100)
+				_putchar(n / 100 % 10 + '0');
+			if (n >= 10)
+				_putchar(n / 10 % 10 + '0');
+			_putchar(n % 10 + '0');
 	}
+
 }
