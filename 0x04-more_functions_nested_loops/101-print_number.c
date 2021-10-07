a2b45e8d8410bde7a3925c772a8db181c56b292f
WILLOT Philippe <willot.ph@gmail.com>
2021-10-07 14:23:53 +0000

    add 5digits numbers

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -19,13 +19,15 @@ void print_number(int n)
 			n -= n * 2;
 			_putchar('-');
 		}
-			if (n >= 1000)
-				_putchar(n / 1000 + '0');
-			if (n >= 100)
-				_putchar(n / 100 % 10 + '0');
-			if (n >= 10)
-				_putchar(n / 10 % 10 + '0');
-			_putchar(n % 10 + '0');
+		if (n >= 10000)
+			_putchar(n / 10000 + '0');
+		if (n >= 1000)
+			_putchar(n / 1000 % 10 + '0');
+		if (n >= 100)
+			_putchar(n / 100 % 10 + '0');
+		if (n >= 10)
+			_putchar(n / 10 % 10 + '0');
+		_putchar(n % 10 + '0');
 	}
 
 }
