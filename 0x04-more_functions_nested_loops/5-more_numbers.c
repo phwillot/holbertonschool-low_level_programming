abf8708c1ede5a258b77362601f15453a41ca188
WILLOT Philippe <willot.ph@gmail.com>
2021-10-07 10:16:07 +0000

    Removed one _putchar

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -11,12 +11,10 @@ void more_numbers(void)
 	int i;
 
 	for (i = 0; i < 15; i++)
+	{
 		if (i >= 10)
-		{
 			_putchar(i / 10 + '0');
-			_putchar(i % 10 + '0');
-		}
-		else
-			_putchar(i + '0');
+		_putchar(i % 10 + '0');
+	}
 	_putchar('\n');
 }
