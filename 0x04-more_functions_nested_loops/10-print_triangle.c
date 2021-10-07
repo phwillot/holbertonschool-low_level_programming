2153a42d741c00c960319ec1ade67336138a152a
WILLOT Philippe <willot.ph@gmail.com>
2021-10-07 07:45:25 +0000

    Add programm triangle

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -10,15 +10,30 @@
 void print_triangle(int size)
 {
 	if (size <= 0)
+	{
 		_putchar('\n');
+	}
 	else
 	{
-		int i, y;
-
-		for (i = 0; i < size; i++)
+		if (size == 1)
 		{
-			for (y = 0; y < size; y++)
+			_putchar('#');
+			_putchar('\n');
+		}
+	}
+
+	if (size > 1)
+	{
+		int i, y, z, taille = size;
+
+		for (i = 1; i <= size; i++)
+		{
+			for (y = taille - 1; y > 0; y--)
+				_putchar(' ');
+
+			for (z = taille - 1; z < size ; z++)
 				_putchar('#');
+			taille--;
 			_putchar('\n');
 		}
 	}
