d689f01ef76f0a627f8c67856e32d835466db48e
WILLOT Philippe <willot.ph@gmail.com>
2021-10-08 13:43:31 +0000

    104-fibonacci.c

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+  * main - Entry point
+  *
+  * Description: Prints the first 50 Fibonacci numbers, starting with 1 and 2
+  *
+  * Return: Always 0 (Success)
+  */
+
+int main(void)
+{
+	long double i, y = 0, z = 1, number;
+
+	for (i = 0; i < 97; i++)
+	{
+		number = y + z;
+		if (i != 96)
+			printf("%0.Lf, ", number);
+		else
+			printf("%0.Lf\n", number);
+		y = z;
+		z = number;
+	}
+	return (0);
+}
