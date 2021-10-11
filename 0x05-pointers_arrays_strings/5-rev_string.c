42c094c0f4e1a18d9147f9f8462350cc4a1da33d
WILLOT Philippe <willot.ph@gmail.com>
2021-10-11 13:38:08 +0000

    5-rev_string.c

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -9,17 +9,20 @@
 
 void rev_string(char *s)
 {
-	int length = 0, i = 0, y = 0;
+	int length = 0, i = 0, y = 0, temp;
 
 	while (s[length] != '\0')
 		length++;
 
 	y = length - 1;
 
-	while (i < length)
+	while (i < y)
 	{
-		s[i] = s[y];
+		temp = s[y];
+		s[y] = s[i];
+		s[i] = temp;
 		i++;
 		y--;
 	}
+	s[length] = '\0';
 }
