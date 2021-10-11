64be0a458cb1aa0c3d8b464c7d0e8624314ea1e9
WILLOT Philippe <willot.ph@gmail.com>
2021-10-11 08:55:03 +0000

    Add program 2

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+  * _strlen - Prints length of a string
+  * @s: pointer to an char
+  *
+  * Return: Length of a string
+  */
+
+int _strlen(char *s)
+{
+	int i = 0, n = 0;
+
+	while (s[i] != '\0')
+	{
+		n++;
+		i++;
+	}
+
+	return (n);
+}
