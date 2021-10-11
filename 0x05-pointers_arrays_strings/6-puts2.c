a1e07b2a634ba11300ca2636bbaaee16575c1de9
WILLOT Philippe <willot.ph@gmail.com>
2021-10-11 11:16:51 +0000

    Fixed for loop

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -11,8 +11,10 @@ void puts2(char *str)
 {
 	int i;
 
-	for (i = 0; str[i] != '\0'; i += 2)
+	for (i = 0; str[i] != '\0'; i++)
 	{
+		if (i % 2 != 0)
+			continue;
 		_putchar(str[i]);
 	}
 	_putchar('\n');
