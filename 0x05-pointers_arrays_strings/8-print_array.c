4708fd57a55f1c60fa977769bd1ec5c23b70eac9
WILLOT Philippe <willot.ph@gmail.com>
2021-10-11 11:52:02 +0000

    Fixed cases <0

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -13,11 +13,12 @@ void print_array(int *a, int n)
 {
 	int i;
 
+	if (n <= 0)
+		printf("\n");
+
 	for (i = 1; i <= n; i++)
 		if (i != n)
 			printf("%d, ", a[i - 1]);
 		else if (i == n)
 			printf("%d\n", a[i - 1]);
-		else
-			printf("\n");
 }
