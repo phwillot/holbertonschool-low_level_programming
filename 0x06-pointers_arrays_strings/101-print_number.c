a7f3b7e42fa36aa923207d5144b858faec22e3c0
WILLOT Philippe <willot.ph@gmail.com>
2021-10-14 10:30:04 +0000

    Optimized program

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include <limits.h>
 /**
   * print_number - Prints an integer
   * @n: Print this integer
@@ -9,7 +8,7 @@
 
 void print_number(int n)
 {
-	unsigned int i = n;
+	unsigned int i = n, div = 1000000000;
 
 	if (n == 0)
 		_putchar('0' + n);
@@ -21,25 +20,12 @@ void print_number(int n)
 			_putchar('-');
 		}
 
-		if (i >= 1000000000)
-			_putchar(i / 1000000000 % 10 + '0');
-		if (i >= 100000000)
-			_putchar(i / 100000000 % 10 + '0');
-		if (i >= 10000000)
-			_putchar(i / 10000000 % 10 + '0');
-		if (i >= 1000000)
-			_putchar(i / 1000000 % 10 + '0');
-		if (i >= 100000)
-			_putchar(i / 100000 % 10 + '0');
-		if (i >= 10000)
-			_putchar(i / 10000 % 10 + '0');
-		if (i >= 1000)
-			_putchar(i / 1000 % 10 + '0');
-		if (i >= 100)
-			_putchar(i / 100 % 10 + '0');
-		if (i >= 10)
-			_putchar(i / 10 % 10 + '0');
+		while (div >= 10)
+		{
+			if (i > div)
+				_putchar(i / div % 10 + '0');
+			div /= 10;
+		}
 		_putchar(i % 10 + '0');
 	}
-
 }
