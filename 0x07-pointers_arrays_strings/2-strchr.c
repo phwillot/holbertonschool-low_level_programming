8ef4d11a82eb4218a13fa6eac2034ca387eb7f68
WILLOT Philippe <willot.ph@gmail.com>
2021-10-18 11:33:34 +0000

    Fixed case with \0 ending

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -13,13 +13,14 @@ char *_strchr(char *s, char c)
 {
 	int i = 0;
 
-	if (c == '\0')
-		return (0);
 	while (s[i] != '\0')
 	{
 		if (s[i] == c)
 			return (s + i);
 		i++;
 	}
+	if (c == '\0')
+		return (s + i);
+
 	return (0);
 }
