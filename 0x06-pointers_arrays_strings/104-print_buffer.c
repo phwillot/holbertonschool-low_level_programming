739887d53bd465f46217e96a74d20a96fdf26362
WILLOT Philippe <willot.ph@gmail.com>
2021-10-19 13:52:20 +0000

    Fix 0 and 1 almost

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -10,7 +10,7 @@
 
 void print_buffer(char *b, int size)
 {
-	int i, buffer = 0x0, j = 8, temp;
+	int i, y = 0, buffer = 0x0, j = 8, temp;
 
 	for (i = 0; i < size; i += 2)
 	{
@@ -37,6 +37,16 @@ void print_buffer(char *b, int size)
 			j += 10;
 			i = temp;
 		}
+	}
+	if (size % 10 != 0)
+	{
+		for (y = 0; y < (25 - (size % 10) - 1); y++)
+			printf(" ");
+		for (i -= (size % 10) + 1; i < size; i++)
+			if (b[i] > 10)
+				printf("%c", b[i]);
+			else
+				printf("%c", '.');
 	}
 	if (i % 10 != 0 || size == 0)
 		printf("\n");
