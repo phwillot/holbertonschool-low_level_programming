34b6235b436a4059b418f22e5937186e63441387
WILLOT Philippe <willot.ph@gmail.com>
2021-10-20 13:58:15 +0000

    Fix task 5

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -8,33 +8,26 @@
 
 int _sqrt_recursion(int n)
 {
-	int i = get_square(n);
+	if (n == 0)
+		return (0);
 
-	if (n < 0)
-		return (-1);
-
-	if (n == 1)
-		return (1);
-
-	return (i);
+	return (get_square(n, 1));
 }
 
 /**
   * get_square - get the index of number
   * @n: Integer
+  * @i: Integer
   *
   * Return: number if (i * i == n)
   */
 
-int get_square(int n)
+int get_square(int n, int i)
 {
-	int i = 0;
+	if (i * i == n)
+		return (i);
+	if (i * i > n)
+		return (-1);
 
-	while (i < n)
-	{
-		if (i * i == n)
-			return (i);
-		i++;
-	}
-	return (-1);
+	return (get_square(n, i + 1));
 }
