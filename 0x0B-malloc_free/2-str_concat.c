27fb4136d722c9e0ac33491bebefc3a6d4f5b9bc
WILLOT Philippe <willot.ph@gmail.com>
2021-10-26 08:38:35 +0000

    Fixed case with one str being NULL

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -14,36 +14,41 @@ char *str_concat(char *s1, char *s2)
 	unsigned int i = 0, y = 0;
 	char *ar;
 
-	if (s1 == NULL || s2 == NULL)
+	if (s1 == NULL && s2 == NULL)
 	{
 		ar = malloc(1);
-		ar[0] = '\0';
 		return (ar);
 	}
-
-	while (s1[i])
-		i++;
-	while (s2[y])
-		y++;
-
+	if (s1 != NULL)
+	{
+		while (s1[i])
+			i++;
+	}
+	if (s2 != NULL)
+	{
+		while (s2[y])
+			y++;
+	}
 	ar = malloc((i * sizeof(char)) + (y * sizeof(char) + 1));
-
 	if (ar == NULL)
 		return (NULL);
-
 	i = 0;
-	while (s1[i])
+	if (s1 != NULL)
 	{
-		ar[i] = s1[i];
-		i++;
+		while (s1[i])
+		{
+			ar[i] = s1[i];
+			i++;
+		}
 	}
 	y = 0;
-	while (s2[y])
+	if (s2 != NULL)
 	{
-		ar[i] = s2[y];
-		y++;
-		i++;
+		while (s2[y])
+		{
+			ar[i] = s2[y];
+			y++, i++;
+		}
 	}
-
 	return (ar);
 }
