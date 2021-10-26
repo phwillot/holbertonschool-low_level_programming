9aa6f9ca155f131c4083b7042dea229dfe054cb0
WILLOT Philippe <willot.ph@gmail.com>
2021-10-26 14:06:43 +0000

    Added 5 tasks (100)

---
+++ 0x0B-malloc_free/100-argstostr.c
@@ -0,0 +1,59 @@
+#include <stdio.h>
+#include "main.h"
+#include <stdlib.h>
+
+/**
+  * argstostr - Concatenates all the arguments of your program.
+  * @ac: Number of arguments
+  * @av: Array of strings containing name of the arguments.
+  *
+  * Return: Pointer to a new string.
+  */
+
+char *argstostr(int ac, char **av)
+{
+	int i, y, compteur = 0;
+	char *ptr;
+
+	if (ac == 0 || av == NULL)
+		return (NULL);
+
+	for (i = 0; av[i]; i++)
+		compteur += _strlen(av[i]) + 2;
+
+	ptr = malloc(compteur);
+	compteur = 0;
+
+	for (i = 0; i < ac; i++)
+	{
+		for (y = 0; av[i][y]; y++)
+		{
+			ptr[compteur] = av[i][y];
+			compteur++;
+		}
+		ptr[compteur] = '\n';
+		compteur++;
+	}
+
+	return (ptr);
+}
+
+/**
+  * _strlen - Prints length of a string
+  * @s: pointer to an char
+  *
+  * Return: Length of a string
+  */
+
+int _strlen(char *s)
+{
+	int i = 0, n = 0;
+
+	while (s[i] != '\0')
+	{
+		n++;
+		i++;
+	}
+
+	return (n);
+}
