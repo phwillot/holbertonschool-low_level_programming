244bbdda57629cd35cdd2aaf8b7234ffa6b29ac0
WILLOT Philippe <willot.ph@gmail.com>
2021-10-28 18:40:06 +0000

    Fixing case removing memory

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -8,31 +8,31 @@
   * @old_size: previous size in bytes
   * @new_size: new size in bytes
   *
-  * Return: (NULL) if new size == oldsize and newsize is equal to zero.
+  * Return: Pointer the new size of memory allocated, NULL if it fails.
   */
 
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
+	unsigned int y;
+
 	if (new_size == old_size)
 		return (ptr);
+
 	if (new_size == 0 && ptr != NULL)
 	{
 		free(ptr);
 		return (NULL);
 	}
 
-	if (ptr == NULL)
-	{
+	if (ptr == NULL || new_size > old_size)
 		ptr = malloc(new_size);
-	}
-
-	if (new_size > old_size)
-	{
-		ptr = malloc(new_size);
-	}
 
 	if (new_size < old_size)
-		free(ptr);
+		for (y = 0; y >= new_size; y--)
+		{
+			free(ptr);
+			ptr = malloc(new_size);
+		}
 
 	return (ptr);
 }
