7dd41e1b5057727a9bb5622e3c500dab364f2e33
WILLOT Philippe <willot.ph@gmail.com>
2021-10-28 18:11:19 +0000

    100-realloc.c

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -24,16 +24,13 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	if (ptr == NULL)
 	{
 		ptr = malloc(new_size);
-		if (ptr == NULL)
-			return (NULL);
 	}
 
 	if (new_size > old_size)
 	{
-		ptr = malloc(sizeof(ptr) * (new_size - old_size));
-		if (ptr == NULL)
-			return (NULL);
+		ptr = malloc(sizeof(ptr) * (new_size));
 	}
+
 	if (new_size < old_size)
 		free(ptr);
 
