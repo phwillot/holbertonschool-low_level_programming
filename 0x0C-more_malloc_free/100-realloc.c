b5b6f8c01444fc46ce7e925f549aa91eddf625fe
WILLOT Philippe <willot.ph@gmail.com>
2021-10-28 16:05:41 +0000

    Add more edge cases

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -17,13 +17,16 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	if (new_size == old_size)
 		return (ptr);
 	if (new_size == 0 && ptr != NULL)
+	{
+		free(ptr);
 		return (NULL);
+	}
 
 	if (ptr == NULL)
-		ptr = malloc(sizeof(ptr) * (new_size));
+		ptr = malloc(new_size);
 
 	if (new_size > old_size)
-		ptr = malloc(sizeof(ptr) * (new_size));
+		ptr = malloc(sizeof(ptr) * (new_size - old_size));
 
 	if (new_size < old_size)
 		free(ptr);
