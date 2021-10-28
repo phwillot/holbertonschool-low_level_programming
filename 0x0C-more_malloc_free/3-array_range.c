f6c1f6e82592938446c8e9b75ce0c37891f588ef
WILLOT Philippe <willot.ph@gmail.com>
2021-10-28 12:00:09 +0000

    3-array_range.c

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -19,13 +19,13 @@ int *array_range(int min, int max)
 	if (min > max)
 		return (NULL);
 
-	ptr = malloc(sizeof(ptr) * (max - min) + 4);
+	ptr = malloc(sizeof(ptr) * (max - min) + 1);
 	if (ptr == NULL)
 		return (NULL);
 
 	difference = max - min;
 
-	for (i = 0; i < difference; i++, min++)
+	for (i = 0; i <= difference; i++, min++)
 		ptr[i] = min;
 
 	return (ptr);
