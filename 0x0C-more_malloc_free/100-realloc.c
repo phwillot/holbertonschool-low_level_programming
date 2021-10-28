b71cb8c059d8547cd2ef8b859bd22473e7fbd20b
WILLOT Philippe <willot.ph@gmail.com>
2021-10-28 16:00:28 +0000

    Add task 100

---
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -0,0 +1,32 @@
+#include "main.h"
+#include <stdlib.h>
+
+
+/**
+  * _realloc - Reallocates a memory block using malloc and free.
+  * @ptr: Void pointer to reallocate memory
+  * @old_size: previous size in bytes
+  * @new_size: new size in bytes
+  *
+  * Return: (NULL) if new size == oldsize and newsize is equal to zero.
+  */
+
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+{
+
+	if (new_size == old_size)
+		return (ptr);
+	if (new_size == 0 && ptr != NULL)
+		return (NULL);
+
+	if (ptr == NULL)
+		ptr = malloc(sizeof(ptr) * (new_size));
+
+	if (new_size > old_size)
+		ptr = malloc(sizeof(ptr) * (new_size));
+
+	if (new_size < old_size)
+		free(ptr);
+
+	return (ptr);
+}
--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -8,4 +8,5 @@ char *str_concat(char *s1, char *s2);
 void *_calloc(unsigned int nmemb, unsigned int size);
 char *_memset(char *s, char b, unsigned int n);
 int *array_range(int min, int  max);
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
 #endif
