1723d553e03d6f168e1c6a0fff393d0c7e7b42c1
WILLOT Philippe <willot.ph@gmail.com>
2021-10-28 12:08:09 +0000

    CHange sized malloc into int

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -19,7 +19,7 @@ int *array_range(int min, int max)
 	if (min > max)
 		return (NULL);
 
-	ptr = malloc(sizeof(ptr) * (max - min) + 1);
+	ptr = malloc(sizeof(int) * (max - min) + 1);
 	if (ptr == NULL)
 		return (NULL);
 
