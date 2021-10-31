1e3e26b669ebd0d4aa35667fe689a83ee0e016c4
WILLOT Philippe <willot.ph@gmail.com>
2021-10-31 10:11:50 +0000

    Added a variable of type struct dog

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -8,19 +8,15 @@
   * @owner: owner of the dog
   */
 
-struct dog
+typedef struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-};
-
+} dog_t;
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
-
-typedef struct dog dog_t;
 dog_t *new_dog(char *name, float age, char *owner);
-
 void free_dog(dog_t *d);
 
 #endif /* _DOG_H_ */
