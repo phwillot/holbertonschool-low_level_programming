9c63c0a6b0717eb1b5855d343da46792450404fb
WILLOT Philippe <willot.ph@gmail.com>
2021-10-31 09:26:29 +0000

    Try to fix the checks

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -13,8 +13,9 @@
 
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	free(d);
 	d = malloc(sizeof(struct dog));
+	if (d == NULL)
+		free(d);
 	d->name = name;
 	d->age = age;
 	d->owner = owner;
