bb442b73309b722cff256dd60bea9ec0da182e11
WILLOT Philippe <willot.ph@gmail.com>
2021-10-31 13:06:19 +0000

    4-new_dog.c

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -13,15 +13,24 @@
 
 dog_t *new_dog(char *name, float age, char *owner)
 {
+	char *nameC = name;
+	char *ownerC = owner;
 	dog_t *dog;
 
 	dog = malloc(sizeof(dog_t));
 	if (dog == NULL)
 		return (NULL);
 
-	dog->name = name;
+	nameC = malloc(sizeof(name));
+	if (nameC == NULL)
+		return (NULL);
+	ownerC = malloc(sizeof(owner));
+	if (ownerC == NULL)
+		return (NULL);
+
+	dog->name = nameC;
 	dog->age = age;
-	dog->owner = owner;
+	dog->owner = ownerC;
 
 	return (dog);
 }
