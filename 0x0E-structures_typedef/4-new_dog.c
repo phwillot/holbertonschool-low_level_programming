7708dcc7c27edb4beb773db1a43b6f5c1473be6d
WILLOT Philippe <willot.ph@gmail.com>
2021-10-31 12:28:09 +0000

    Try to copy name and owner

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -17,16 +17,21 @@ dog_t *new_dog(char *name, float age, char *owner)
 	dog_t *copy;
 
 	dog = malloc(sizeof(dog_t));
-	copy = malloc(sizeof(dog_t));
 	if (dog == NULL)
+		return (NULL);
+	copy = malloc(sizeof(dog_t));
+	if (copy == NULL)
 		return (NULL);
 
 	dog->name = name;
-	copy->name = dog->name;
+	if (dog->name != NULL)
+		copy->name = dog->name;
+
 	dog->age = age;
 
 	dog->owner = owner;
-	copy->owner = dog->owner;
+	if (dog->owner != NULL)
+		copy->owner = dog->owner;
 
 	return (dog);
 }
