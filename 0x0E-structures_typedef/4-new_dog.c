5e74f9a1e55821583ac0596960b0bcb142a1eceb
WILLOT Philippe <willot.ph@gmail.com>
2021-10-31 14:00:29 +0000

    trying to fix

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -24,7 +24,6 @@ dog_t *new_dog(char *name, float age, char *owner)
 	dog->name = _strdup(name);
 	if (dog->name == NULL)
 	{
-		free(dog->name);
 		free(dog);
 		return (NULL);
 	}
@@ -33,7 +32,7 @@ dog_t *new_dog(char *name, float age, char *owner)
 	dog->owner = _strdup(owner);
 	if (dog->owner == NULL)
 	{
-		free(dog->owner);
+		free(dog->name);
 		free(dog);
 		return (NULL);
 	}
