1f4591219e4acdb7267919b6e57e923e46c10758
WILLOT Philippe <willot.ph@gmail.com>
2021-10-31 14:22:41 +0000

    Tring to fix

--- 0x0E-structures_typedef/5-free_dog.c
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -12,10 +12,16 @@
 void free_dog(dog_t *d)
 {
 	if (d == NULL)
+		return;
+
+	if (d->name == NULL)
+	{
+		free(d);
+		return;
+	}
+	if (d->owner == NULL)
 	{
 		free(d->name);
-		free(d->owner);
 		free(d);
 	}
-
 }
