bee12ac42a45c769336de097564b947e99e76e28
WILLOT Philippe <willot.ph@gmail.com>
2021-11-03 11:33:14 +0000

    Checking if cmp is NULL

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -13,14 +13,16 @@ int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i = 0;
 
-	if (size <= 0)
-		return (-1);
-
-	for (; i < size; i++)
+	if (cmp)
 	{
-		if (cmp(array[i]))
-			return (i);
-	}
+		if (size <= 0)
+			return (-1);
 
+		for (; i < size; i++)
+		{
+			if (cmp(array[i]))
+				return (i);
+		}
+	}
 	return (-1);
 }
