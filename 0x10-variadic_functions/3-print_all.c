c688689272c4a1ba31fca7c162465023ce5071fc
WILLOT Philippe <willot.ph@gmail.com>
2021-11-04 15:27:30 +0000

    Added last task mandatory

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -12,6 +12,13 @@ void print_all(const char * const format, ...)
 	int i = 0, y = 0;
 	va_list args;
 	char *characters = "cifs";
+	ch_t ch[] = {
+		{'c', ch_char},
+		{'i', int_char},
+		{'f', float_char},
+		{'s', string_char},
+	};
+
 
 	va_start(args, format);
 
@@ -19,9 +26,11 @@ void print_all(const char * const format, ...)
 	{
 		while (characters[y])
 		{
-			if (format[i] == characters[y])
+			if (format[i] == ch[y].ch)
 			{
-				printf("%d, ", va_arg(args, int));
+				ch[y].c(args);
+				if (format[i + 1] != '\0')
+					printf(", ");
 			}
 			y++;
 		}
@@ -31,4 +40,51 @@ void print_all(const char * const format, ...)
 
 	va_end(args);
 	printf("\n");
+}
+
+/**
+ * ch_char - Prints a character based on the argument
+ * @arg: list of variadic arguments
+ * Return: void
+ */
+
+void ch_char(va_list arg)
+{
+	printf("%c", va_arg(arg, int));
+}
+
+/**
+ * int_char - Prints an integer based on the argument
+ * @arg: list of variadic arguments
+ *
+ * Return:void
+ */
+
+void int_char(va_list arg)
+{
+	printf("%d", va_arg(arg, int));
+}
+
+/**
+  * float_char - Prints a float based on the argument
+  * @arg: list of variadic arguments
+  *
+  * Return: void
+  */
+
+void float_char(va_list arg)
+{
+	printf("%f", va_arg(arg, double));
+}
+
+/**
+  * string_char - Prints a string based one the argument
+  * @arg: list of variadic arguments
+  *
+  * Return: void
+  */
+
+void string_char(va_list arg)
+{
+	printf("%s", va_arg(arg, char *) ? va_arg(arg, char *) : "(nil)");
 }
--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -11,4 +11,15 @@ void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
 
+typedef struct ch
+{
+	char ch;
+	void (*c)(va_list);
+} ch_t;
+
+void ch_char(va_list);
+void int_char(va_list);
+void float_char(va_list);
+void string_char(va_list);
+
 #endif /* _VARIADIC_FUNCTIONS_H_ */
