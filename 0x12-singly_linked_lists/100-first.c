1effc6a834bc3b6129b5babdae3f11dda9a9ccc8
Philippe <willot.ph@gmail.com>
2021-11-13 16:52:55 +0100

    Added betty documentation

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,6 +1,12 @@
 #include "lists.h"
 
-void __attribute__ ((constructor)) premain()
+/**
+  * premain - Prints a string before the main program
+  *
+  * Return: Void
+  */
+
+void __attribute__ ((constructor)) premain(void)
 {
 	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
 }
