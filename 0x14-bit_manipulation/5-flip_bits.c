e1df9d5e50773acdd012f6c2cf74897e66687c41
WILLOT Philippe <willot.ph@gmail.com>
2021-11-18 17:05:06 +0000

    5-flip_bits.c

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -16,7 +16,7 @@ unsigned int flip_bits(unsigned long int n, unsigned long int m)
 	i = 0;
 	while (i < 32)
 	{
-		if (((n >> i) & 1) != ((m >> i) & 1))
+		if (((n >> i) & 1) ^ ((m >> i) & 1))
 			count++;
 		i++;
 	}
