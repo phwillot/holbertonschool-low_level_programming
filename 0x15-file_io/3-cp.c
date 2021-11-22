bfb84b5dadd883000016ae4668acdd7b25703f92
WILLOT Philippe <willot.ph@gmail.com>
2021-11-22 15:50:30 +0000

    try to fix bug

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -14,43 +14,35 @@
 
 int main(int ac, char **av)
 {
-	int fd, fd2, number;
+	int fd, fd2, number, testWrite;
 	char buf[BUFSIZE];
 
 	if (ac != 3)
-	{
-		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
-		exit(97);
-	}
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n"), exit(97);
 
 	fd = open(av[1], O_RDONLY);
 	if (fd == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]);
-		exit(98);
-	}
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]), exit(98);
+
 	fd2 = open(av[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
 	if (fd2 == -1)
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]), exit(99);
+
+	while ((number = read(fd, buf, BUFSIZE)) != 0)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]);
-		exit(99);
+		if (number == -1)
+			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[2]), exit(98);
+
+		testWrite = write(fd2, buf, number);
+
+		if (testWrite == -1)
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]), exit(99);
 	}
 
-	number = read(fd, buf, 1024);
-	if (write(fd2, buf, number) == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]);
-		exit(99);
-	}
 	if (close(fd) == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", close(fd));
-		exit(100);
-	}
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", close(fd)), exit(100);
 	if (close(fd2) == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", close(fd2));
-		exit(100);
-	}
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", close(fd2)), exit(100);
+
 	return (0);
 }
