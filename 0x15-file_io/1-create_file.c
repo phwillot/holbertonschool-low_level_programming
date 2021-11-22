4e44e4b86ae2dbc19e2c650872087bcf848cd092
WILLOT Philippe <willot.ph@gmail.com>
2021-11-22 13:14:53 +0000

    test fix bug

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -16,6 +16,13 @@ int create_file(const char *filename, char *text_content)
 	if (!filename)
 		return (-1);
 
+	if (!text_content)
+	{
+		fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+		close(fd);
+		return (1);
+	}
+
 	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 	if (fd == -1)
 		return (-1);
