8e62a67de116960ca2e8f442173dd5a3d0745d0e
WILLOT Philippe <willot.ph@gmail.com>
2021-11-22 16:06:22 +0000

    fix fd

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,7 +1,5 @@
 #include "main.h"
 
-#define BUFSIZE 1024
-
 /**
   * main - Entry point
   * @ac: number of arguments.
@@ -15,7 +13,7 @@
 int main(int ac, char **av)
 {
 	int fd, fd2, characters, writen, closed;
-	char buf[BUFSIZE];
+	char buf[1024];
 
 	if (ac != 3)
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n"), exit(97);
@@ -28,7 +26,7 @@ int main(int ac, char **av)
 	if (fd2 == -1)
 		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]), exit(99);
 
-	while ((characters = read(fd, buf, BUFSIZE)) != 0)
+	while ((characters = read(fd, buf, 1024)) != 0)
 	{
 		if (characters == -1)
 			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[2]), exit(98);
