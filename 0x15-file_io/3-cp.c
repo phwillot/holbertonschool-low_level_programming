bbe9562721db495a6a0f5078d2b5f2f02eadff1f
WILLOT Philippe <willot.ph@gmail.com>
2021-11-22 14:46:19 +0000

    Fix stderr bug

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -25,7 +25,6 @@ int main(int ac, char **av)
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]);
 		exit(98);
 	}
-
 	fd = open(av[1], O_RDONLY, 0664);
 	fd2 = open(av[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
 	if (fd == -1 || fd2 == -1)
@@ -33,22 +32,24 @@ int main(int ac, char **av)
 		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]);
 		exit(99);
 	}
-
 	buf = malloc(sizeof(char) * 1024);
 	if (!buf)
 		return (-1);
 
 	number = read(fd, buf, 1024);
-
 	if (write(fd2, buf, number) == -1)
 		return (-1);
 
 	if (close(fd) == -1)
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d", close(fd));
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", close(fd));
+		exit(100);
+	}
 	if (close(fd2) == -1)
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d", close(fd));
-
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", close(fd2));
+		exit(100);
+	}
 	free(buf);
-
 	return (0);
 }
