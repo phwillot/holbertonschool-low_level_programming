65ef546776b1ac355e64855edc14555d8d299367
WILLOT Philippe <willot.ph@gmail.com>
2021-11-22 11:45:28 +0000

    Initial commit of this repo

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+  * read_textfile - Reads a text file and prints it to the POSIX standard ouput
+  * @filename: filetext
+  * @letters: Numberf of letters it should print
+  *
+  * Return: Number of letters, if filename is NULL and write fails (0)
+  */
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int fd;
+	char buf[1024];
+	size_t i = 0;
+
+	if (!filename)
+		return (0);
+
+	fd = open(filename, O_RDONLY, 0600);
+	if (fd == -1)
+		return (0);
+
+	i = read(fd, buf, letters);
+	buf[letters + 1] = '\0';
+
+	write(STDOUT_FILENO, buf, i);
+	close(fd);
+
+	return (i);
+}
---
+++ 0x15-file_io/README.md
@@ -0,0 +1,5 @@
+# 0x15-file_io
+
+## In this project, I learned about file I/O.
+
+### Description of the tasks:
