0be74ddca920ed348ea11c6cdc63fcdca4bd9784
WILLOT Philippe <willot.ph@gmail.com>
2021-11-22 14:54:34 +0000

    Trying to fix error output

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -20,17 +20,13 @@ int main(int ac, char **av)
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 		exit(97);
 	}
-	if (!av[1])
-	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]);
-		exit(98);
-	}
+
 	fd = open(av[1], O_RDONLY, 0664);
 	fd2 = open(av[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
 	if (fd == -1 || fd2 == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]);
-		exit(99);
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]);
+		exit(98);
 	}
 	buf = malloc(sizeof(char) * 1024);
 	if (!buf)
@@ -38,8 +34,10 @@ int main(int ac, char **av)
 
 	number = read(fd, buf, 1024);
 	if (write(fd2, buf, number) == -1)
-		return (-1);
-
+	{
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]);
+		exit(99);
+	}
 	if (close(fd) == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", close(fd));
