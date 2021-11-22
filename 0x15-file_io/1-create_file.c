9ace6d3cd738153f8fcc14db90409213077cd95e
WILLOT Philippe <willot.ph@gmail.com>
2021-11-22 13:07:57 +0000

    1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -37,8 +37,10 @@ int create_file(const char *filename, char *text_content)
 		buf[i] = text_content[i];
 		i++;
 	}
+	if (i == 0)
+		write(fd, "", 1);
 
-	if (write(fd, buf, i) == -1)
+	else if (write(fd, buf, i) == -1)
 		return (-1);
 
 	close(fd);
