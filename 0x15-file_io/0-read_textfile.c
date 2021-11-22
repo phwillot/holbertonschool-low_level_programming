5db799d658160d93fcc0ee289156c0a9d11202d8
WILLOT Philippe <willot.ph@gmail.com>
2021-11-22 12:04:35 +0000

    Adding one edge case

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -16,14 +16,11 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	if (!filename)
 		return (0);
 
-	fd = open(filename, O_RDONLY, 0600);
+	fd = open(filename, O_RDONLY);
 	if (fd == -1)
 		return (0);
 
 	i = read(fd, buf, letters);
-
-	buf[letters + 1] = '\0';
-
 	close(fd);
 
 	if (write(STDOUT_FILENO, buf, i) == -1)
