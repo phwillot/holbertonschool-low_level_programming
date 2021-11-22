7666fcea1d5328abffa82bedeb0dba8f25c1f2aa
WILLOT Philippe <willot.ph@gmail.com>
2021-11-22 16:23:57 +0000

    try to fix bug

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -26,7 +26,7 @@ int main(int ac, char **av)
 	if (fd2 == -1)
 		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]), exit(99);
 
-	while ((characters = read(fd, buf, 1024)) >= 0)
+	while ((characters = read(fd, buf, 1024)) > 0)
 	{
 		if (characters == -1)
 			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[2]), exit(98);
