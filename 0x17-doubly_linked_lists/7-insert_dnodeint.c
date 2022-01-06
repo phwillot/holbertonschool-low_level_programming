69615f2448e42e9f810090e9191e0d93cb738da0
Philippe WILLOT <willot.ph@gmail.com>
2022-01-06 05:19:39 -0800

    test

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -16,9 +16,8 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	dlistint_t *new, *temp;
 	unsigned int i = 0;
 
-	if (h == NULL || !*h)
+	if (h == NULL)
 		return (NULL);
-
 	if (idx > sizeLinkedList(*h))
 		return (NULL);
 
