e8e8e729ee46fe9d03c2ebc4e9de3865bf594eda
WILLOT Philippe <willot.ph@gmail.com>
2022-01-06 11:35:57 +0000

    try to fix task 7

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -38,6 +38,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		i++;
 	}
 	new->next = temp;
+	temp->next->prev = new;
 	temp = *h;
 	i = 0;
 	while (i < (idx - 1))
