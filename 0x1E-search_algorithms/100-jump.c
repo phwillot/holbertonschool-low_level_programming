dc434885ff9f963e8c1abb7e543c2c6bab7db213
Philippe Willot <willot.ph@gmail.com>
2022-05-03 14:50:21 +0200

    bug fix task 100

--- 0x1E-search_algorithms/100-jump.c
+++ 0x1E-search_algorithms/100-jump.c
@@ -22,7 +22,7 @@ int jump_search(int *array, size_t size, int value)
 		{
 			j += step;
 			printf("Value checked array[%ld] = [%d]\n", i, array[i]);
-			if (array[j] > value)
+			if (array[j] >= value)
 			{
 				printf("Value found between indexes [%ld] and [%ld]\n", i, j);
 				while (i <= j)
